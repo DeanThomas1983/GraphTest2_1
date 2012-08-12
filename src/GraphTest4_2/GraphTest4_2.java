9d8c956479e4fecf6c838509820dd7bb6a6d424d
Dean Thomas <dean.plymouth@googlemail.com>
2012-08-12 15:09:02 +0100

    Finish coding exercise 4.2

--- src/GraphTest4_2/GraphTest4_2.java
+++ src/GraphTest4_2/GraphTest4_2.java
@@ -8,9 +8,14 @@ import Generic.SimpleGraphView;
 import edu.uci.ics.jung.algorithms.layout.CircleLayout;
 import edu.uci.ics.jung.algorithms.layout.Layout;
 import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Paint;
+import java.awt.Stroke;
+import javax.swing.JFrame;
 import org.apache.commons.collections15.Transformer;
 
 /**
@@ -31,16 +36,39 @@ public class GraphTest4_2 {
                 new BasicVisualizationServer<Integer, String>(layout);
         vv.setPreferredSize(new Dimension(350, 350));
         //  Setup a new vertex to paint transformer...
-        
-        /*
         Transformer<Integer, Paint> vertexPaint = 
                 new Transformer<Integer, Paint>()
                 {
-                    public Paint transfrom(Integer i)
+                    @Override
+                    public Paint transform(Integer i)
                     {
                         return Color.GREEN;
                     }
                 };
-                */
+        //  Set up a new stroke Transformer for the edges
+        float dash[] = {10.0f};
+        final Stroke edgeStroke = 
+                new BasicStroke(1.0f, BasicStroke.CAP_BUTT, 
+                    BasicStroke.JOIN_MITER, 10.0f, dash, 0.0f);
+        Transformer<String, Stroke> edgeStrokeTransformer = 
+                new Transformer<String, Stroke>()
+                {
+                    @Override
+                    public Stroke transform(String s)
+                    {
+                        return edgeStroke;
+                    }
+                };
+        vv.getRenderContext().setVertexFillPaintTransformer(vertexPaint);
+        vv.getRenderContext().setEdgeStrokeTransformer(edgeStrokeTransformer);
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
+        
+        JFrame frame = new JFrame("Simple graph view 2");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(vv);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
