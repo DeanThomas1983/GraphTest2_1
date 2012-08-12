07bb395f262646b321161c67172e5d738c3a679c
Dean Thomas <dean.plymouth@googlemail.com>
2012-08-12 16:11:05 +0100

    Add code for "GraphTest5_2".

---
+++ src/GraphTest5_2/GraphTest5_2.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GraphTest5_2;
+
+import Samples.Basic.InteractiveGraphView;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author dean
+ */
+public class GraphTest5_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InteractiveGraphView sgv = new InteractiveGraphView();
+        
+        Layout<Integer, String> layout = new CircleLayout(sgv.g);
+        layout.setSize(new Dimension(300,300));
+        VisualizationViewer<Integer, String> vv =
+                new VisualizationViewer<Integer, String>(layout);
+        vv.setPreferredSize(new Dimension(350,350));
+        //  Show vertex and edge labels
+        vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller());
+        vv.getRenderContext().setEdgeLabelTransformer(new ToStringLabeller());
+        //  Create a graph mouse and add it to the visualization component
+        DefaultModalGraphMouse gm =
+                new DefaultModalGraphMouse();
+        gm.setMode(ModalGraphMouse.Mode.TRANSFORMING);
+        vv.setGraphMouse(gm);
+        
+        //  Control mouse mode by keyboard
+        vv.addKeyListener(gm.getModeKeyListener());
+        
+        JFrame frame = new JFrame("Interactive Graph View 1");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(vv);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
